cce320049b0a27f5a81ebed3ad10050e43596475
Talib Mattar <talib.mattar@emwealthtech.com>
2021-03-03 20:04:04 +0800

    updates

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import Sidebar from './Sidebar'
 
-function App() {
+const items = [
+  {name: 'home', label:'Home'},
+  {
+    name: 'billing',
+    label: 'Billing',
+    items: [
+      {name:'statements', label: 'Statements'},
+      {name:'reports', label: 'Reports'},
+     ]
+  },
+  {
+    name: 'settings', 
+    label:'Settings',
+    items: [{name: 'profile', label: 'Profile'}]
+  }
+]
+
+function App () {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Sidebar items={items} />
     </div>
-  );
+  )
 }
 
 export default App;
